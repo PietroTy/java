5ea5df631690a9a72eac9510779d4fec5432c922
PietroTy <pietro.turcimm@gmail.com>
2024-09-19 14:16:12 -0300

    jvdone

--- Locadora/src/App.java
+++ Locadora/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Cliente cliente = new Cliente();
+        cliente.CNH = 12111111;
+        cliente.CPF = 12111111;
+        cliente.nome = "alberto";
+        Carro carro = new Carro();
+        carro.ano = 2001;
+        carro.cor = "azul";
+        carro.modelo = "alberto";
+        Locacao locacao = new Locacao();
+        locacao.locacao = 010122001;
+        locacao.protocolo = 12111111;
+        locacao.validade = 010122002;
+
     }
 }
